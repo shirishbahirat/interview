473c154648261dc598f58e0bdda9ed6eb39d54b8
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-23 21:14:35 -0600

    solved problems

--- 33.cpp
+++ 33.cpp
@@ -7,6 +7,21 @@ using namespace std;
 
 class Solution {
 public:
+
+    int get_pivot(vector<int>& nums)
+    {
+        if (nums.empty())
+        {
+            return -1;
+        }
+
+        if (nums.size() < 2)
+        {
+            return 0;
+        }
+
+    }
+
     int search(vector<int>& nums, int target) {
         
 
@@ -23,6 +38,6 @@ int main(int argc, char const *argv[])
     Solution *obj = new Solution();
 
     int index = obj->search(arr, 7);
-    
+
     return 0;
 }
