e44ae0a077ccc4bb6e6691ad5e7576e69ac86e89
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-23 20:26:52 -0600

    solved problems

--- pattern-bin-search.cpp
+++ pattern-bin-search.cpp
@@ -13,7 +13,33 @@ int main(const int argc, const char* argv[])
 
 	int len = sizeof(arr)/sizeof(arr[0]);
 
-	
+	int left = 0;
+
+	int right = len - 1;
+
+	int val = 10;
+
+	while (left < right)
+	{
+
+		int mid = left + (right - left)/2;
+
+		if (val == arr[mid])
+		{
+			cout << mid << endl;
+			return;
+		}
+
+		if (val > arr[mid])
+		{
+			left = mid + 1;
+		} 
+		else if ( val < arr[mid])
+		{
+			right = mid;
+		}
+
+	}
 
 
 
