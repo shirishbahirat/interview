f766d831c0c0afab374bd8a6a4c8c6946906798e
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-25 21:12:38 -0600

    solved problems

--- 34.cpp
+++ 34.cpp
@@ -16,10 +16,12 @@ Example 1:
 
 Input: nums = [5,7,7,8,8,10], target = 8
 Output: [3,4]
+
 Example 2:
 
 Input: nums = [5,7,7,8,8,10], target = 6
 Output: [-1,-1]
+
 Example 3:
 
 Input: nums = [], target = 0
