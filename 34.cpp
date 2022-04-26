b4546d19753c9e37a6bb5a05d46932526650facf
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-25 21:08:14 -0600

    solved problems

---
+++ 34.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+//Title: Find First and Last Position of Element in Sorted Array
+
+using namespace std;
+
+class Solution
+{
+public:
+    int pivotIndex(vector<int> &nums)
+    {
+         return 0;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution *obj = new Solution();
+    return 0;
+}
