bf5c31eacc21da61b64bbe2565820f1c46d4c738
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-25 20:45:49 -0600

    solved problems

--- 74.cpp
+++ 74.cpp
@@ -5,17 +5,17 @@
 
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int pivotIndex(vector<int> &nums)
-    {
-         return 0;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
     }
 };
-
 int main(int argc, char const *argv[])
 {
     Solution *obj = new Solution();
+
+    vector<vector<int>> matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+
     return 0;
 }
