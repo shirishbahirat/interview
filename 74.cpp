8eb6136db44bb40c7196011620aaa0acd6048288
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-25 21:05:43 -0600

    solved problems

--- 74.cpp
+++ 74.cpp
@@ -64,7 +64,7 @@ int main(int argc, char const *argv[])
 
     vector<vector<int>> matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
 
-    cout << obj->searchMatrix(matrix, 13) << endl;
+    cout << obj->searchMatrix(matrix, 7) << endl;
 
     return 0;
 }
