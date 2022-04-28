62393a559c9d676b248cc925908b739a9ef1fa17
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-27 21:06:17 -0600

    solved problems

---
+++ 981.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+//Title: Time Based Key-Value Store
+
+using namespace std;
+
+class Solution
+{
+public:
+    int pivotIndex(vector<int> &nums)
+    {
+         return 0;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution *obj = new Solution();
+    return 0;
+}
