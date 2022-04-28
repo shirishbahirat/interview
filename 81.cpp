f6f1ce20fe9f0e3bb16109a879f79c1163010c96
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-27 19:58:03 -0600

    solved problems

--- 81.cpp
+++ 81.cpp
@@ -31,15 +31,119 @@ using namespace std;
 
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        
 
-        return true;
+    int bin_search(vector<int>& nums, int left, int right, int target)
+    {
+
+        while (left <= right)
+        {
+
+            int mid = left + (right - left)/2;
+
+            if (target == nums[mid])
+            {
+                return mid;
+            }
+
+            if (target > nums[mid])
+            {
+                left = mid + 1;
+            } 
+            else if ( target < nums[mid])
+            {
+                right = mid;
+            }
+
+            if (left == right)
+            {
+                if (nums[left] == target)
+                    return left;
+
+                return -1;
+            }            
+
+        }
+
+        return -1;
+
+    }
+
+    int get_pivot(vector<int>& nums)
+    {
+
+
+        if (nums.empty())
+        {
+            return -1;
+        }
+
+        int i = 0;
+
+        for (i = 0; i < nums.size() - 1; ++i)
+        {
+            if (nums[i+1] < nums[0])
+            {
+                return i + 1;
+            } 
+        }
+
+        return i;
+    }
+
+    int search(vector<int>& nums, int target) {
+        
+        if (nums.size() == 1)
+        {
+            if (target == nums[0]) return true;
+
+            return false;
+        }
+
+        int mid = get_pivot(nums);
+
+        if (mid < 0)
+            return false;
+
+
+        int idy = mid;
+
+        while ((idy >= 0) && (nums[idy] == nums[mid]))
+        {
+
+            results[0] = idy;
+            --idy;
+        }
+
+
+        int idx = bin_search(nums,0, idy, target);
+
+        if (idx >= 0)
+        {
+            return idx;
+        }
+
+        int idy = bin_search(nums, idy+1, nums.size() - 1, target);
+
+        if (idy >= mid)
+        {
+            return idy;
+        }
+
+        return -1;
     }
 };
 
+
 int main(int argc, char const *argv[])
 {
+
+    vector<int> arr = {4,5,6,7,0,1,2};
+
     Solution *obj = new Solution();
+
+    int index = obj->search(arr, 2);
+
+    cout << index << endl;
+
     return 0;
 }
