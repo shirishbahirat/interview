6211c84516b1deb80ead4729a1682537fb6fb8e4
Shirish Bahirat <shirishbahirat@comcast.net>
2022-04-27 20:50:36 -0600

    solved problems

---
+++ 611.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+//Title: Valid Triangle Number
+
+using namespace std;
+
+class Solution
+{
+public:
+    int pivotIndex(vector<int> &nums)
+    {
+         return 0;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution *obj = new Solution();
+    return 0;
+}
