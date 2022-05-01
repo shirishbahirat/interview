0d4788e1b9a06a2928b6ebce95811a167d57bca6
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-01 08:52:37 -0600

    solved problems

--- 278.cpp
+++ 278.cpp
@@ -7,10 +7,33 @@ using namespace std;
 
 class Solution {
 public:
+
+    int bad_version = 4;
+
+    int arr[] = {1,2,3,4,5,6,7};
+
+    bool isBadVersion(int version)
+    {
+        if (version == bad_version) return true;
+
+        return false;
+    }
+
     int firstBadVersion(int n) {
         
+        int left = 0, right = n;
 
-        return 0;
+        while (left < right)
+        {
+            int mid = left + (left - right)/2;
+
+            if (isBadVersion(mid)) return true;
+
+            left = mid + 1;
+
+        }
+
+        return left;
     }
 };
 
