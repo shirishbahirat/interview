695ee49350cc28521e6cefe96d07151da5ee38a1
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-02 21:25:01 -0600

    solved problems

--- 1011.cpp
+++ 1011.cpp
@@ -7,8 +7,51 @@ using namespace std;
 
 class Solution {
 public:
+
+    bool feasible(int capacity,  vector<int>& weights, int days)
+    {
+        int day = 1;
+        int total = 0;
+
+        for (int i = 0; i < weights.size(); ++i)
+        {
+            total += weights[i];
+
+            if (total > capacity)
+            {
+                total = weight;
+                day += 1;
+
+                if (day > days)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     int shipWithinDays(vector<int>& weights, int days) {
         
+        int left = max(weights);
+        int right = min(weights);
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (feasible(mid, weights, days))
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+
     }
 };
 
