11ffba5daa9614c178d53e125bca2e3faff9e448
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-03 21:19:49 -0600

    solved problems

--- 105.cpp
+++ 105.cpp
@@ -5,12 +5,21 @@
 
 using namespace std;
 
-class Solution
-{
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
 public:
-    int pivotIndex(vector<int> &nums)
-    {
-         return 0;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        
+
+        return nullptr;
     }
 };
 
