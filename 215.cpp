14e1e6cc96dc9ba884613620b52e4b93a227112e
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-03 20:51:58 -0600

    solved problems

---
+++ 215.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+//Title: Kth Largest Element in an Array
+
+using namespace std;
+
+class Solution
+{
+public:
+    int pivotIndex(vector<int> &nums)
+    {
+         return 0;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution *obj = new Solution();
+    return 0;
+}
