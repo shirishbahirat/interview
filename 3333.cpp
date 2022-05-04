fe89c3db09af553ef084baa04a78b7bb81c5ba7a
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-04 17:35:25 -0600

    solved problems

--- 3333.cpp
+++ 3333.cpp
@@ -11,8 +11,31 @@ public:
     int ExactOrMin(vector<int> &nums, int val)
     {
 
-        
-         return val;
+        if (nums.size() == 0)
+            return -1;
+
+        int ans = -1;
+
+        int start = 0;
+        int end = nums.size();
+
+        while (start > end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == val)
+                return val;
+
+            if (val > nums[mid])
+                start = mid + 1;
+            else if (val < nums[mid])
+                end = mid;
+
+        }
+
+
+
+         return ans;
     }
 };
 
