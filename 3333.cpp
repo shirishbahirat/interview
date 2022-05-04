cbe414512c6828ccfbfdd471999782791068d5fd
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-04 17:52:50 -0600

    solved problems

--- 3333.cpp
+++ 3333.cpp
@@ -52,7 +52,7 @@ int main(int argc, char const *argv[])
 
     vector<int> nums = {2,5,7,12,17,19,22};
 
-    cout << obj->ExactOrMin(nums, 20) << endl;
+    cout << obj->ExactOrMin(nums, 3) << endl;
 
     return 0;
 }
