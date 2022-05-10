ed2f8ff270e426cbb32b921b1d216998cf2a25af
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-09 20:58:24 -0600

    solved problems

--- wfs.cpp
+++ wfs.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <stack>
 
 
@@ -8,22 +9,8 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 
-	vector<vector<string>> graph = {{"b","c"},{"m", "b"},{"j", "k", "c"}, {"l", "m"}, {"j","n, o, p"}};
+	map<string, vector<string>> graph;
 
-	stack <string> stc;
-
-	if (graph.empty()) return 0;
-
-	stc.push(graph[0][0]);
-
-	while (!stc.empty())
-	{
-		string c = stc.top();
-		stc.pop();
-
-		cout << c << endl;
-
-	}
 
 
 
