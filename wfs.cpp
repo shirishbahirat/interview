c4b69348ad4a17538c119594cf47b4678c0778a8
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-09 21:00:30 -0600

    solved problems

--- wfs.cpp
+++ wfs.cpp
@@ -12,7 +12,12 @@ int main(int argc, char* argv[])
 	map<string, vector<string>> graph;
 
 	graph["a"] = {"b", "c"};
-	
+	graph["b"] = {"d"};
+	graph["c"] = {"e"};
+	graph["d"] = {"f"};
+	graph["e"] = { };
+	graph["f"] = { };
+
 
 
 
