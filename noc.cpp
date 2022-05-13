2544e11b6038bcd5120b3d72cbb14d2750c5af75
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-13 09:43:55 -0600

    solved problems

--- noc.cpp
+++ noc.cpp
@@ -224,8 +224,8 @@ int main(int argc, const char* argv[])
 	}
 
 
-	uint32_t gtx = [2, 5];
-	uint32_t gty = [2, 5];
+	uint32_t gtx[] = {2, 5};
+	uint32_t gty[] = {2, 5};
 
 
 	noc[gtx[0]][gty[0]]->launch_n(noc[gtx[0] - 1][gty[0]]);
