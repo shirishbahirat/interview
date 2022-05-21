a31ba3f05abc5fb9b82d23cdfabc6480683299b0
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-20 20:31:02 -0600

    solved problems

--- invll.cpp
+++ invll.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 
+using namespace std;
+
+struct node {
+	node* next;
+	int data;
+
+	node(int d): data(d), next(nullptr) {}
+};
 
 int main(int argc, char* argv[])
 {
+
+	node* n = new node(10);
+
+	node* head = n;
+
+	n = new node(11);
+
+	head->next = n;
+
+	n = new node(12);
+
+	head->next->next = n;
+
+	node* temp = head;
+
+	while(temp)
+	{
+		cout << temp->data << endl;
+		temp = temp->next;
+	}
 
 
 
