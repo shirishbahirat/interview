22e9751329b6caf6d778ec54cc174e390d59a5dd
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-20 22:13:29 -0600

    solved problems

--- mergesort.cpp
+++ mergesort.cpp
@@ -5,6 +5,14 @@ using namespace std;
 int mergesort(int arr[], int low, int high)
 {
 
+	if (low < high)
+	{
+		int mid = low + (high - low)/2;
+
+		mergesort(arr, low, mid);
+		mergesort(arr, mid + 1, high);
+
+	}
 
 
 	return 0;
@@ -14,6 +22,10 @@ int mergesort(int arr[], int low, int high)
 int main(int argc, char* argv[])
 {
 
+	int arr[] = {4, 3, 8, 5, 11, 2};
+	int len = sizeof(arr)/sizeof(arr[0]);
+
+	mergesort(arr, 0, len - 1);
 
 	return 0;
 }
