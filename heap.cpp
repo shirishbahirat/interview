69eb692845653ab2a3b46ede55b29df58403a2a8
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-22 08:45:33 -0600

    solved problems

--- heap.cpp
+++ heap.cpp
@@ -2,10 +2,39 @@
 
 using namespace std;
 
+void swap(int arr[], int a, int b)
+{
+
+	if (a == b) return;
+
+	int temp = arr[a];
+
+	arr[a] = arr[b];
+
+	arr[b] = temp;
+
+	return;
+
+}
+
+void trickle_up(int arr[], int idx)
+{
+
+	if (parent <= 0) return;
+
+	int parent = (idx - 1)/2;
+
+	if (arr[idx] > arr[parent]) swap(arr, idx, parent);
+
+	trickle_up(arr, parent);
+
+	return;
+}
+
 int main(int argc, char* argv[])
 {
 
-	int arr[] = {8, 7, 4, 5, 3, 2 , 1};
+	int arr[] = {8, 7, 4, 5, 3, 2 , 1, 15};
 
 
 
