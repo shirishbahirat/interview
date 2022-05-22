73e1d8c1848659a5c94494959a4a582c3d6716d8
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-21 18:32:08 -0600

    solved problems

--- 2064.cpp
+++ 2064.cpp
@@ -37,23 +37,23 @@ public:
     int minimizedMaximum(int n, vector<int>& quantities) {
         
 
-     int left = *max_element(quantities.begin(), quantities.end());
-        int right = reduce(quantities.begin(), quantities.end());
+    int left = *max_element(quantities.begin(), quantities.end());
+    int right = reduce(quantities.begin(), quantities.end());
 
-        while (left < right)
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if (feasible(mid, quantities, n))
         {
-            int mid = left + (right - left) / 2;
-            if (feasible(mid, quantities, n))
-            {
-                right = mid;
-            }
-            else
-            {
-                left = mid + 1;
-            }
+            right = mid;
         }
+        else
+        {
+            left = mid + 1;
+        }
+    }
 
-        return left;
+    return left;
 
     }
 };
