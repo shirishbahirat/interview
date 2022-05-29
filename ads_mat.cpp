2c41e82dee334343480a6c42d8f7720a3fa784b2
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-29 09:41:34 -0600

    solved problems

---
+++ ads_mat.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+a->b->c
+a->b->d
+
+
+int main(int argc, const char* argv[])
+{
+
+	int arr[] = {{0,1,1,0,0,0},
+				 {0,0,0,1,0,0},
+				 {0,0,0,0,1,0},
+				 {0,0,0,0,0,1},
+				 {0,0,0,0,0,0},
+				 {0,0,0,0,0,0}};
+
+	char nme[] = {"a","b","c","d","e","f"};
+
+
+
+	return 0;
+}
