b534a49703e43427ebb8c6be7287b422b7a1a4b8
Shirish Bahirat <shirishbahirat@comcast.net>
2022-05-28 21:21:03 -0600

    solved problems

--- 240.cpp
+++ 240.cpp
@@ -24,7 +24,7 @@ public:
             if (target > nums[mid])
             {
                 left = mid + 1;
-            } 
+            }
             else if ( target < nums[mid])
             {
                 right = mid;
@@ -36,7 +36,7 @@ public:
                     return true;
 
                 return false;
-            }            
+            }
 
         }
 
