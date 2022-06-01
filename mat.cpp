2f60107e3c3866d49d8ccb4fc31f6ddf0eabdafa
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-01 16:51:57 -0600

    solved problems

--- mat.cpp
+++ mat.cpp
@@ -19,14 +19,11 @@ int main(int argc, char* argv[])
 	};
 
 
-	for (int i = 1; i < 4; ++i)
+	for (int i = 0; i < 4 - 1; ++i)
 	{
-		for (int j = i+1; j <= 4; ++j)
-		{
-			int t = mat[i][j];
-			mat[i][j] = mat[j][i];
-			mat[j][i] = t;
-		}
+		int t = mat[0][i];
+		mat[0][i] = mat[i][0];
+		mat[i][0] = t;
 	}
 
 	for (int i = 0; i < 4; ++i)
