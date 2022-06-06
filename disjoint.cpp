6830764d6dedf257f66d7c8d68c2242cfac58989
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-06 14:27:54 -0600

    solved problems

--- disjoint.cpp
+++ disjoint.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int arr[] = {-1,0,0,0,1,-1,5,5,6};
 
-int get_abs_root(int arr[], int base)
+void get_abs_root(int arr[], int base)
 {
-	if (arr[base] == -1) return base;
+	if (arr[base] == -1) cout << base << endl;
 
 	get_abs_root(arr, arr[base]);
 
-	return -1;
+	return;
 
 }
 
 int main(int argc, const char* argv[])
 {
 
-	cout << get_abs_root(arr, 4) << endl;
+	get_abs_root(arr, 4);
 
 	return 0;
 }
