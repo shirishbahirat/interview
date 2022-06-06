cc039a05b6cdc6f49b69f792c52952c6211e2a3d
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-06 14:28:27 -0600

    solved problems

--- disjoint.cpp
+++ disjoint.cpp
@@ -6,7 +6,11 @@ int arr[] = {-1,0,0,0,1,-1,5,5,6};
 
 void get_abs_root(int arr[], int base)
 {
-	if (arr[base] == -1) cout << base << endl;
+	if (arr[base] == -1) 
+	{
+		cout << base << endl;
+		return;
+	}
 
 	get_abs_root(arr, arr[base]);
 
