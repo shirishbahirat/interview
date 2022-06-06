f29aab62c181b0d0d106298cad72b9e8a6dae3dd
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-06 13:50:46 -0600

    solved problems

---
+++ presum.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[] = {8,3,4,5,7,8,9,-1,3, -4, 5};
+
+int size = sizeof(arr)/sizeof(arr[0]);
+
+int psum[size];
+
+void pre_sum(void)
+{
+
+	psum[0] = arr[0];
+
+	for (int i = 1; i < size; ++i)
+	{
+		psum[i] = psum[i-1] + psum[i];
+	}
+
+}
+
+
+int main(int argc, const char* argv[])
+{
+
+	pre_sum();
+
+	for (auto i: psum)
+	{
+		cout << i << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
