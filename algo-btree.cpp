a314cb182ea19afe5987f4802371813ac1fef9c9
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-06 19:28:44 -0600

    solved problems

--- algo-btree.cpp
+++ algo-btree.cpp
@@ -19,6 +19,22 @@ int bin_search(vector<int> arr, int lo, int hi, int target)
 	return -1;
 }
 
+int bin_search(vector<int> arr, int target)
+{
+	int lo = 0, hi = arr.size() - 1;
+
+	while (lo < hi)
+	{
+		int mid = lo + (hi - lo) / 2;
+
+		if (arr[mid] == target) return mid;
+		else if (arr[mid] > target) hi = mid;
+		else if (arr[mid] < target) lo = mid + 1;
+	}
+
+	return - 1;
+}
+
 int main(int argc, const char* argv[])
 {
 
