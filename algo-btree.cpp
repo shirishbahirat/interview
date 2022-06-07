bb4486b9d303e6f6704078442b7d5f1c82d80822
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-06 19:10:27 -0600

    solved problems

--- algo-btree.cpp
+++ algo-btree.cpp
@@ -7,7 +7,7 @@ int bin_search(int arr[], int lo, int hi, int target)
 	if ((lo == hi) && arr[lo] == target) return lo;
 	if ((lo == hi) && arr[lo] != target) return -1;
 
-	if (lo > hi) return -1;
+	if (lo >= hi) return -1;
 
 	int mid = lo + (hi - lo) / 2;
 
