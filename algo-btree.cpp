b71aab7a9afa0e2eee1e2fcca49355bb8f1e1288
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-06 19:05:20 -0600

    solved problems

--- algo-btree.cpp
+++ algo-btree.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int bin_search(int arr[], int lo, int hi, int number)
 {
-	if ((lo == hi) && arr[lo] == number) return lo;
-	if ((lo == hi) && arr[lo] != number) return -1;
+	//if ((lo == hi) && arr[lo] == number) return lo;
+	//if ((lo == hi) && arr[lo] != number) return -1;
+
+	if (lo < high) return -1;
 
 	int mid = lo + (hi - lo) / 2;
 
@@ -22,9 +24,7 @@ int main(int argc, const char* argv[])
 	int arr[] = {5,6,8,9,12,15};
 	int size = sizeof(arr)/sizeof(arr[0]);
 
-
 	cout << bin_search(arr, 0, size - 1, 6) << endl;
-
 
 	return 0;
 }
