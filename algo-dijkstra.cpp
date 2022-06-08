bb734e718bf1f2bd1ac79251a7e96f9d524cdabc
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-08 12:05:20 -0600

    solved problems

--- algo-dijkstra.cpp
+++ algo-dijkstra.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 #define V 9
 
-int min_distance(int dist[], bool sptSet[])
+int min_distance(int dist[], bool spt_set[])
 {
 
 	int min = INT_MAX, min_index;
 
 	for (int v = 0; v < V; v++)
 	{
-		if (sptSet[v] == false && dist[v] <= min)
+		if (spt_set[v] == false && dist[v] <= min)
 			min = dist[v], min_index = v;
 	}
 
@@ -30,21 +30,21 @@ void dijkstra(int graph[V][V], int src)
 {
 	int dist[V]; 
 
-	bool sptSet[V];
+	bool spt_set[V];
 
 	for (int i = 0; i < V; i++)
-		dist[i] = INT_MAX, sptSet[i] = false;
+		dist[i] = INT_MAX, spt_set[i] = false;
 
 	dist[src] = 0;
 
 	for (int count = 0; count < V - 1; count++) {
-		int u = min_distance(dist, sptSet);
+		int u = min_distance(dist, spt_set);
 
-		sptSet[u] = true;
+		spt_set[u] = true;
 
 		for (int v = 0; v < V; v++)
 
-			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
+			if (!spt_set[v] && graph[u][v] && dist[u] != INT_MAX
 				&& dist[u] + graph[u][v] < dist[v])
 				dist[v] = dist[u] + graph[u][v];
 	}
