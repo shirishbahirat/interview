774eaf00043344be6ae795f731a8001b32cd65e4
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-07 18:37:09 -0600

    solved problems

--- algo-kruskals.cpp
+++ algo-kruskals.cpp
@@ -9,22 +9,20 @@ int main(int argc, const char* argv[])
 
 	vector < pair <int, pair <int, int>>> edge; 
 
-	edge.push_back({0, {1, 4}});
-	edge.push_back({0, {7, 8}});
-	edge.push_back({0, {1, 4}});
-    edge.push_back({0, {7, 8}});
-    edge.push_back({1, {2, 8}});
-    edge.push_back({1, {7, 11}});
-    edge.push_back({2, {3, 7}});
-    edge.push_back({2, {8, 2}});
-    edge.push_back({2, {5, 4}});
-    edge.push_back({3, {4, 9}});
-    edge.push_back({3, {5, 14}});
-    edge.push_back({4, {5, 10}});
-    edge.push_back({5, {6, 2}});
-    edge.push_back({6, {7, 1}});
-    edge.push_back({6, {8, 6}});
-    edge.push_back({7, {8, 7}});
+	edge.push_back({4, {0, 1}});
+	edge.push_back({8, {0, 7}});
+    edge.push_back({8, {1, 2}});
+    edge.push_back({11, {1, 7}});
+    edge.push_back({7, {2, 3}});
+    edge.push_back({2, {2, 8}});
+    edge.push_back({4, {2, 5}});
+    edge.push_back({9, {3, 4}});
+    edge.push_back({14, {3, 5}});
+    edge.push_back({10, {4, 5}});
+    edge.push_back({2, {5, 6}});
+    edge.push_back({1, {6, 7}});
+    edge.push_back({6, {6, 8}});
+    edge.push_back({7, {7, 8}});
 
 
 	return 0;
