febccca83c2f1fe2b2aa8f0e566ebd203269ba1f
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-07 18:28:28 -0600

    solved problems

--- algo-kruskals.cpp
+++ algo-kruskals.cpp
@@ -11,27 +11,22 @@ int main(int argc, const char* argv[])
 
 	edge.push_back({0, {1, 4}});
 	edge.push_back({0, {7, 8}});
+	edge.push_back({0, {1, 4}});
+    edge.push_back({0, {7, 8}});
+    edge.push_back({1, {2, 8}});
+    edge.push_back({1, {7, 11}});
+    edge.push_back({2, {3, 7}});
+    edge.push_back({2, {8, 2}});
+    edge.push_back({2, {5, 4}});
+    edge.push_back({3, {4, 9}});
+    edge.push_back({3, {5, 14}});
+    edge.push_back({4, {5, 10}});
+    edge.push_back({5, {6, 2}});
+    edge.push_back({6, {7, 1}});
+    edge.push_back({6, {8, 6}});
+    edge.push_back({7, {8, 7}});
 
 
 	return 0;
 }
 
-/*
-
-
-  g.addEdge(0, 1, 4);
-    g.addEdge(0, 7, 8);
-    g.addEdge(1, 2, 8);
-    g.addEdge(1, 7, 11);
-    g.addEdge(2, 3, 7);
-    g.addEdge(2, 8, 2);
-    g.addEdge(2, 5, 4);
-    g.addEdge(3, 4, 9);
-    g.addEdge(3, 5, 14);
-    g.addEdge(4, 5, 10);
-    g.addEdge(5, 6, 2);
-    g.addEdge(6, 7, 1);
-    g.addEdge(6, 8, 6);
-    g.addEdge(7, 8, 7);
-
- */
