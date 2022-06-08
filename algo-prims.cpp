e7d9f29e7db89779647a4022f05e5c6a0b5bb09a
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-08 10:01:40 -0600

    solved problems

--- algo-prims.cpp
+++ algo-prims.cpp
@@ -10,49 +10,49 @@ using namespace std;
 int main(int argc, const char* argv[])
 {
 
-	vector < pair <int, int> > edge;
+	vector < pair <int, int> > edge(10,{});
 
-    edge.push_back(make_pair(0,4));
-    edge.push_back(make_pair(1,4));
+    edge[1].push_back(make_pair(0,4));
+    edge[0].push_back(make_pair(1,4));
 
-    edge.push_back(make_pair(0,8));
-    edge.push_back(make_pair(7,8));
+    edge[7].push_back(make_pair(0,8));
+    edge[0].push_back(make_pair(7,8));
 
-    edge.push_back(make_pair(1,8));
-    edge.push_back(make_pair(2,8));
+    edge[2].push_back(make_pair(1,8));
+    edge[1].push_back(make_pair(2,8));
 
-    edge.push_back(make_pair(1,11));
-    edge.push_back(make_pair(7,11));
+    edge[7].push_back(make_pair(1,11));
+    edge[1].push_back(make_pair(7,11));
 
-    edge.push_back(make_pair(2,7));
-    edge.push_back(make_pair(3,7));
+    edge[3].push_back(make_pair(2,7));
+    edge[2].push_back(make_pair(3,7));
 
-    edge.push_back(make_pair(2,2));
-    edge.push_back(make_pair(8,2));
+    edge[8].push_back(make_pair(2,2));
+    edge[2].push_back(make_pair(8,2));
 
-    edge.push_back(make_pair(2,4));
-    edge.push_back(make_pair(5,4));
+    edge[5].push_back(make_pair(2,4));
+    edge[2].push_back(make_pair(5,4));
 
-    edge.push_back(make_pair(3,9));
-    edge.push_back(make_pair(4,9));
+    edge[4].push_back(make_pair(3,9));
+    edge[3].push_back(make_pair(4,9));
 
-    edge.push_back(make_pair(3,14));
-    edge.push_back(make_pair(5,14));
+    edge[5].push_back(make_pair(3,14));
+    edge[3].push_back(make_pair(5,14));
 
-    edge.push_back(make_pair(4,10));
-    edge.push_back(make_pair(5,10));
+    edge[5].push_back(make_pair(4,10));
+    edge[4].push_back(make_pair(5,10));
 
-    edge.push_back(make_pair(5,2));
-    edge.push_back(make_pair(6,2));
+    edge[6].push_back(make_pair(5,2));
+    edge[5].push_back(make_pair(6,2));
 
-    edge.push_back(make_pair(6,1));
-    edge.push_back(make_pair(7,1));
+    edge[7].push_back(make_pair(6,1));
+    edge[6].push_back(make_pair(7,1));
 
-    edge.push_back(make_pair(6,6));
-    edge.push_back(make_pair(8,6));
+    edge[8].push_back(make_pair(6,6));
+    edge[6].push_back(make_pair(8,6));
 
-    edge.push_back(make_pair(7,7));
-    edge.push_back(make_pair(8,7));
+    edge[8].push_back(make_pair(7,7));
+    edge[7].push_back(make_pair(8,7));
 
     typedef pair <int, int> p;
 
