5c0f301d5991e7222f505d89159804a66c0408f1
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-08 21:51:19 -0600

    solved problems

--- algo-heap.cpp
+++ algo-heap.cpp
@@ -2,11 +2,102 @@
 
 using namespace std;
 
-int main(int argc, const char* argv[])
+void swap(int arr[], int a, int b)
 {
 
+	if (a == b) return;
 
+	int temp = arr[a];
 
+	arr[a] = arr[b];
+
+	arr[b] = temp;
+
+	return;
+
+}
+
+void trickle_up(int arr[], int idx)
+{
+
+	if (idx <= 0) return;
+
+	int parent = (idx - 1)/2;
+
+	if (arr[idx] > arr[parent]) swap(arr, idx, parent);
+
+	trickle_up(arr, parent);
+
+	return;
+}
+
+void trickle_down(int arr[], int idx, int len)
+{
+	int left = 2*idx + 1;
+	int right = 2*idx + 2;
+
+	int max = idx;
+
+	if (left < len && arr[left] > arr[max]) max = left;
+	if (right < len && arr[right] > arr[max]) max = right;
+
+	if (max != idx)
+	{
+		swap(arr, max, idx);
+		trickle_down(arr, max, len);
+	}
+
+	return;
+}
+
+void heap_sort(int arr[], int len)
+{
+
+	int parent = (len - 1)/2;
+
+	for (int i = parent; i >= 0; --i)
+	{
+		trickle_down(arr, i, len);
+	}
+
+	for (int i = len; i >= 0; --i)
+	{
+		swap(arr, i, 0);
+		trickle_down(arr, 0, i);
+	}
+
+	for (int i = 0; i <= len; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+}
+
+int main(int argc, char* argv[])
+{
+
+	int arr[] = {8, 7, 4, 5, 3, 2 , 1, 15};
+
+	trickle_up(arr, 7);
+
+	for (auto i : arr)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+
+	int arr1[] = {1, 8, 7, 4, 5, 3, 2 , 0};
+
+	trickle_down(arr1, 0, 7);
+
+	for (auto i : arr1)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+
+	heap_sort(arr, 7);
 
 	return 0;
 }
