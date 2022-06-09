877f019e02045cfd276dc85e9e57b2292b883ad4
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-09 14:06:09 -0600

    solved problems

---
+++ algo-msort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void mergesort(int arr[], int lo, int hi)
+{
+	if (hi <= lo) return;
+
+	int mid = lo + (hi - lo) / 2;
+
+	mergesort(arr, lo, mid);
+	mergesort(arr, mid+1, hi);
+
+
+}
+
+
+int main(int argc, char* argv[])
+{
+
+	int arr[] = {4, 3, 8, 5, 11, 2};
+	int len = sizeof(arr)/sizeof(arr[0]);
+
+	mergesort(arr, 0, len - 1);
+
+	for (int i = 0; i < len; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
