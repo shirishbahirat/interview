92ab452536b0f3a29e4bb9b312a28e017cf5c1a8
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-10 21:20:52 -0600

    solved problems

---
+++ algo-bt-transversal.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+struct tree {
+	int data;
+	tree* left;
+	tree* right;
+
+	tree(int d): data(d), left(nullptr), right(nullptr) {}
+	~tree() {}
+};
+
+
+int main(int argc, const char* argv[])
+{
+
+	tree* head = new tree(10);
+
+	head->left = new tree(9);
+	head->right = new tree(9);
+
+
+	head->left->left = new tree(7);
+	head->left->right = new tree(4);
+
+	head->right->left = new tree(11);
+	head->right->right = new tree(12);
+
+
+
+
+
+
+
+	return 0;
+}
