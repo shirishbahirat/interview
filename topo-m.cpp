99f4f00d85c004f005dcf07c472640e821bf1ca9
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-18 08:59:09 -0600

    solved problems

--- topo-m.cpp
+++ topo-m.cpp
@@ -11,9 +11,7 @@ int main(int argc, const char* argv[])
 	vector <int> nodes = {0,1,2,3,4,5,6};
 
 	vector <int> parent(7, -1);
-
 	vector <int> degree = {0, 1, 2, 2, 3, 3};
-
 	queue <int> topo;
 	map <int, vector<int>> graph = {
 		{0, {1, 2}},
