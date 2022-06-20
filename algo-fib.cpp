3acfeb851492b134faa0d621cf3e01a0d35c9e7f
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-20 16:37:35 -0600

    solved problems

--- algo-fib.cpp
+++ algo-fib.cpp
@@ -4,7 +4,20 @@
 
 using namespace std;
 
-vector <int> fib_sequence(7, -1);
+vector <int> fib_sequence(10, 0);
+
+int fib(int n)
+{
+
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+
+	if (fib_sequence[n]) return fib_sequence[n];
+
+	fib_sequence[n] = fib(n - 1) + fib(n-2);
+
+	return fib_sequence[n];
+}
 
 int main(int argc, const char* argv[])
 {
