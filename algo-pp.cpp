7b5e8ea4f7385dd851f11d58e9fc55942ca8f9f9
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-24 19:47:53 -0600

    solved problems

--- algo-pp.cpp
+++ algo-pp.cpp
@@ -38,7 +38,7 @@ int main(int argc, const char* argv[])
 	{
 		for (int i = 0; i < 3; ++i)
 		{
-			cout << max[j][i] << " " << *(*(mat + j) + i) << " ";
+			cout << max[j][i] << " " << *(*(max + j) + i) << " ";
 		}
 	}
 	cout << endl;
