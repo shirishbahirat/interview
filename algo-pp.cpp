5006612511cc61fbdea8ee8ee8d1a577a3ea4547
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-25 10:41:20 -0600

    solved problems

--- algo-pp.cpp
+++ algo-pp.cpp
@@ -25,7 +25,7 @@ void prnt(int** arr, int size)
 int main(int argc, const char* argv[])
 {
 
-	cout << sizeof(long int) << endl;
+	cout << sizeof(float) << endl;
 
 	cout << argc << " " << argv[0] << endl;
 
