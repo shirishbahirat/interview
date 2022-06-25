c50c8313f6996eb7ba05da80e4eba6448cab3a05
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-24 19:45:09 -0600

    solved problems

--- algo-pp.cpp
+++ algo-pp.cpp
@@ -30,7 +30,16 @@ int main(int argc, const char* argv[])
 
 	int mat[2][3] = {{1,2,3},{4,5,6}};
 
-	int max[2][3] = {1,2,3,4,5,6};
+	int max[2][3] = {10,20,30,40,50,60};
+
+	for (int j = 0; j < 2; ++j)
+	{
+		for (int i = 0; i < 3; ++i)
+		{
+			cout << max[j][i] << " ";
+		}
+	}
+	cout << endl;
 
 
 	int *m = mat[1];
