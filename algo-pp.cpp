d010e2529d6a18c818223abce64366084b36e13a
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-24 19:46:32 -0600

    solved problems

--- algo-pp.cpp
+++ algo-pp.cpp
@@ -32,11 +32,13 @@ int main(int argc, const char* argv[])
 
 	int max[2][3] = {10,20,30,40,50,60};
 
+	int b = 0;
+
 	for (int j = 0; j < 2; ++j)
 	{
 		for (int i = 0; i < 3; ++i)
 		{
-			cout << max[j][i] << " ";
+			cout << max[j][i] << " " << (max + b++) << " ";
 		}
 	}
 	cout << endl;
