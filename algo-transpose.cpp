65265825b3e43aaa8472894cd6d3dd4f48f58d92
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-26 11:13:20 -0600

    solved problems

--- algo-transpose.cpp
+++ algo-transpose.cpp
@@ -13,7 +13,7 @@ int main(int argc, const char* argv[])
 	{
 		for (int i = 0; i < 3; ++i)
 		{
-			cout << max[j][i] << " " << *(*(max + j) + i) << " ";
+			cout << *(*(max + j) + i) << " ";
 		}
 	}
 
