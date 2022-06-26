d4fa1a71301b3d51d20ccfb043eec3ba82cf821b
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-26 11:13:48 -0600

    solved problems

--- algo-transpose.cpp
+++ algo-transpose.cpp
@@ -7,9 +7,9 @@ int main(int argc, const char* argv[])
 {
 
 
-	int max[2][3] = {10,20,30,40,50,60};
+	int max[2][3] = {10,20,30,40,50,60,70,80,90};
 
-	for (int j = 0; j < 2; ++j)
+	for (int j = 0; j < 3; ++j)
 	{
 		for (int i = 0; i < 3; ++i)
 		{
