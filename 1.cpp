5aca9c637012c57516836c432913ee65e3a68dd6
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-28 15:22:08 -0600

    solved problems

---
+++ 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+
+	int arr[] = {10, 15, 3, 7};
+
+	int size = sizeof(arr)/sizeof(arr[0]);
+
+	int k = 17;
+
+	map <int, int> distance;
+
+	for (int i = 0; i < size; ++i)
+	{
+		int temp = 17 - arr[i];
+		map[temp] = 
+	}
+
+
+	return 0;
+
+}
