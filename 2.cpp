eaea7546d56f2d9238d0548b1cd9f8a53d71c218
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-28 15:15:08 -0600

    solved problems

--- 2.cpp
+++ 2.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main(int argc, const char* argv[])
 {
 	int arr[] = {2,3,4,5,7,8};
+
 	int size = sizeof(arr)/sizeof(arr[0]);
+	
 	int mult = 1;
 
 	for (int i = 0; i < size; ++i)
