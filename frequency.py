8a64bec2b26fdcedc41a26823ef527edac6324d2
Shirish Bahirat <shirishbahirat@comcast.net>
2022-06-29 13:30:57 -0600

    solved problems

--- frequency.py
+++ frequency.py
@@ -2,10 +2,34 @@
 
 core_count = 64
 
-clock speed = 3.2
+clock_speed = 3.2
 
-bis = 64
+bits = 64
 
-per_core_memory_processing = 3.2 * bits/8 * 2
+ddr_pins = 1
+
+ddr6_per_pin_bw_GBps = 18/8
+
+gen6_x16_BW_GBps_duplex = 256
+
+number_of_dimm = 8
+
+bw_gbpes_per_dimmm = 12.8
+
+per_core_memory_processing = clock_speed * bits/8 * 2
 
 print ("per core memory BW GB/s", per_core_memory_processing)
+
+print (core_count, "core memory BW GB/s", per_core_memory_processing * core_count)
+
+print ("pcie total BW with 128 lanes GB/s", per_core_memory_processing*128/16)
+
+print ("per core PCIE BW GB/s", per_core_memory_processing*128/16/core_count)
+
+network_bw_800GBps = 800/8
+
+print("network BW total GB/w", network_bw_800GBps)
+
+print("per core network BW GB/w", network_bw_800GBps/core_count)
+
+print(bw_gbpes_per_dimmm * number_of_dimm)
