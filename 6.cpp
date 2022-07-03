f7c6839f3bfbe659d92715b3d2d2014cde457cd8
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-03 08:15:53 -0600

    solved problems

--- 6.cpp
+++ 6.cpp
@@ -10,10 +10,20 @@ struct node {
 	int index;
 };
 
+node* insert(node* head, int index)
+{
 
+
+	return head;
+}
 
 int main(int argc, char* argv[])
 {
+
+	node* head = nullptr;
+
+
+
 
 	cout << (0x1 ^ 0x3) << endl;
 	cout << (0x0 ^ 0x3) << endl;
