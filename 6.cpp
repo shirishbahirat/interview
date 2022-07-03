8d5095620e45d36a4fb13300b36f58163d849ce8
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-03 08:06:34 -0600

    solved problems

---
+++ 6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+struct node {
+	node* both;
+	int index;
+};
+
+int main(int argc, char* argv[])
+{
+
+	cout << 3 ^ 3 << endl;
+
+    return 0;
+}
