cea9da184ce4e487b413a755c1e013053c2f7ba3
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-03 19:17:16 -0600

    solved problems

--- 6.cpp
+++ 6.cpp
@@ -16,7 +16,7 @@ struct node {
 node* prev = nullptr;
 node* next = nullptr;
 
-node* insert(node* head, int index)
+node* insert(node* head, int index, node* prev)
 {
 	if (head == nullptr)
 	{
@@ -42,9 +42,9 @@ int main(int argc, char* argv[])
 	node* prev = 0x0;
 	node* next = 0x0;
 
-	head = insert(head, 10);
+	head = insert(head, 10, prev);
 
-	head = insert(head, 20);
+	head = insert(head, 20, prev);
 
 	cout << head->index << endl;
 
