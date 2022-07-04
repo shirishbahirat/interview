d6478050cdae1919c8f072066082cea95ab36d59
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-03 21:58:39 -0600

    solved problems

--- 6.cpp
+++ 6.cpp
@@ -7,16 +7,22 @@ using namespace std;
 
 struct node {
 	node* both;
+	node* next;
+	node* prev;
 	int index;
 
-	node(int d):index(d){}
+	node(int d):index(d), 
+				both(nullptr),
+				next(nullptr),
+				prev(nullptr){}
 	~node(){}
 };
 
-node* pre;
-node* nxt;
+node* pre = nullptr;
+node* nxt = nullptr;
+node* head = nullptr;
 
-node* insert(node* head, int index)
+void insert(int index)
 {
 	if (head == nullptr)
 	{
@@ -24,54 +30,35 @@ node* insert(node* head, int index)
 
 		head = pre;
 
-		return head;
+		return;
 	}
-
-	node* curr = head;
-
-	while(curr->both)
-	{
-		uint64_t addr_prev =  reinterpret_cast<uint64_t>(pre);
-		uint64_t add_curr =  reinterpret_cast<uint64_t>(curr->both);
-		uint64_t addr_next = (addr_prev ^ add_curr);
-
-		node* next =  reinterpret_cast<node*>(addr_next);
-
-		curr = next;
-
-		cout << curr->index << " ";
-	}
-
-	cout << endl;
 
 	node* next = new node(index);
 
-	next->both = pre;
-
 	uint64_t addr_prev =  reinterpret_cast<uint64_t>(pre->both);
-	uint64_t add_next =  reinterpret_cast<uint64_t>(curr);
+	uint64_t add_next =  reinterpret_cast<uint64_t>(next);
 
 	uint64_t addr_both = (addr_prev ^ add_next);
 
 	pre->both = reinterpret_cast<node*>(addr_both);
 
-	return head;
+	next->both = pre;
+
+	pre = next;
+
+	return;
 }
 
 int main(int argc, char* argv[])
 {
 
-	node* head = nullptr;
-
-	head = insert(head, 10);
-
-	head = insert(head, 20);
-
-	head = insert(head, 30);
+	insert(10);
 
 	cout << head->index << endl;
 
+	insert(20);
 
+	cout << head->index << endl;
 
     return 0;
 }
