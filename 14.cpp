a5b19f2562f0d3741b999cec4eb3367fb22c1c43
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-10 19:15:31 -0600

    solved problems

--- 14.cpp
+++ 14.cpp
@@ -13,24 +13,21 @@ int main(int argc, const char* argv[])
     int circle = 0;
     int square = 0;
 
-   for (int i = 0; i < 1000; ++i)
+   for (int i = 0; i < 10000; ++i)
    {
    		double x = (double)rand() / (RAND_MAX + 1.0);
    		double y = (double)rand() / (RAND_MAX + 1.0);
 
    		double rs = (x*x) + (y*y);
 
-   		cout << rs << endl;
-
-   		if (rs > 1)
-   		  square++;
-   		else
+   		if (rs < 1)
    		  circle++;
+
+   		square++;
 
 	}
 
-	cout << square << " " << circle << " " << (double)circle/(double)square << endl;
-
+	cout << square << " " << circle << " " << 4*(double)circle/(double)square << endl;
 
 	return 0;
 }
