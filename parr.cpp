6aa1fcbddababb66d9d9e3e8667b755fe7250b75
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-10 21:13:11 -0600

    solved problems

---
+++ parr.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void loop(int arr*, int len)
+{
+
+	for (int i = 0; i < len; ++i)
+	{
+		cout << arr[i] << endl;
+	}
+
+}
+
+
+int main(int argc, const char argv[])
+{
+
+	int arr[] = {1,2,3,4,5,6,7,8};
+
+	int len = sizeof(arr)/sizeof(arr[0]);
+
+	loop(arr, len);
+
+	return 0;
+}
