70b9ed135aa0539d746645f03e91352c2f763e42
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-10 21:19:53 -0600

    solved problems

--- parr.cpp
+++ parr.cpp
@@ -39,7 +39,9 @@ int main(int argc, const char* argv[])
 
 	int arr[] = {1,2,3,4,5,6,7,8};
 
-	int *parr = &arr[0]; 
+	int *parr = &arr[0];
+
+	int **pparr = &parr;
 
 	int len = sizeof(arr)/sizeof(arr[0]);
 
