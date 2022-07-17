0b37c7e617a9f9708c97fa210ac599331ab0084a
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-17 11:11:11 -0600

    solved problems

--- cpu.cpp
+++ cpu.cpp
@@ -14,9 +14,9 @@ dpush() {
     git add .
     if [ "$1" != "" ]
     then
-        git commit --date='2022-$1-$2 11:11:11' --file comment.txt
+        git commit --date='2022-07-17 11:11:11' --file comment.txt
     else
-        git commit --file comment.txt
+        git commit --date='2022-07-17 11:11:11' --file comment.txt
     fi
     git push
 }
