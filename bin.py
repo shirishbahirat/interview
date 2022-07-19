a2d8432072d761fc9b24ff98c76d307c7ddf212d
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-19 12:42:59 -0600

    solved problems

--- bin.py
+++ bin.py
@@ -1,11 +1,25 @@
 from scipy.stats import binom
+import matplotlib.pyplot as plt
 
 qd = n = 64
 p = die = 1/16
 
-sum = 0
-for i in range(200):
-    sum += binom.pmf(i, n, p)
-    if sum >= .999:
-        print (i)
+lim = []
+prob = []
 
+
+for p in range(10, 256):
+    sum = 0
+    for i in range(32):
+        sum += binom.pmf(i, n, 1/p)
+        prob.append(binom.pmf(i, n, 1/p))
+        if sum >= .999:
+            print (i)
+            lim.append(i)
+            sum = -20000
+
+
+plt.plot(lim)
+plt.ylabel('QoS .999')
+plt.xlabel('resources')
+plt.show()
