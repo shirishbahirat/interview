29cfe15dd93c83bd7681ee1a09d8f5dbe95cba71
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-19 17:43:48 -0600

    solved problems

--- qsim.py
+++ qsim.py
@@ -67,7 +67,7 @@ class model(object):
                   'thresh': 1000000 - 1, 
                   'meas': [0 for _ in range(dmns)]}
 
-        self.exec = 4
+        self.exec = 10
 
         self.per_dmn_cmd_cnt = [0 for _ in range(dmns)]
 
