b6fa131ae2a62ca76c8ea0dfcae577c19b736de7
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-19 16:12:36 -0600

    solved problems

--- bin.py
+++ bin.py
@@ -31,6 +31,8 @@ p2 = 1 /(1 - 0.95**(1/n2))
 p3 = 1 /(1 - 0.95**(1/n3))
 p4 = 1 /(1 - 0.95**(1/n4))
 
+
+
 sum = 0
 for i in range(32):
     sum += binom.pmf(i, n1, 1/p1)
@@ -38,6 +40,29 @@ for i in range(32):
         print(i)
         sum = -20000
 
+
+sum = 0
+for i in range(32):
+    sum += binom.pmf(i, n2, 1/p2)
+    if sum >= .999:
+        print(i)
+        sum = -20000
+
+
+sum = 0
+for i in range(32):
+    sum += binom.pmf(i, n3, 1/p3)
+    if sum >= .999:
+        print(i)
+        sum = -20000
+
+
+sum = 0
+for i in range(32):
+    sum += binom.pmf(i, n4, 1/p4)
+    if sum >= .999:
+        print(i)
+        sum = -20000
 
 
 print(p1, p2, p3, p4, p1+p2+p3+p4)
