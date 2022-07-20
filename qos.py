4744fcc84dc3b3005615c0085817f4fcf4a3ee5e
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-20 13:41:56 -0600

    solved problems

--- qos.py
+++ qos.py
@@ -21,7 +21,9 @@ def get_qos(p1, n1):
 	    if sum >= .99999 and qos[3] == 0:
 	        qos[3] = i*10
 
-	print(qos, (1 - 1/p1)**n1, n1)
+	#print(qos, (1 - 1/p1)**n1, n1)
+
+	return qos[0]
 
 
 p1 = 128 #qd
@@ -43,3 +45,41 @@ p4 = 32 #qd
 n4 = 1/(log(1 - (1/p4), 0.95)) # die
 
 get_qos(p4, n4)
+
+plt.figure()
+
+for j in range(4,512,32):
+	qo = []
+	res = []
+	for i in range(1,10):
+		n1 = 1/(log(1 - (1/p1), i/10 - 0.01))
+		idx = get_qos(j, n1)
+		qo.append(idx)
+		res.append(int(n1))
+		plt.plot(res, qo)
+
+plt.show()
+
+
+dmsn = 5
+
+res_cnt = [2 for _ in range(dmsn)]
+
+util = [1/dmsn for _ in range(dmsn)]
+
+weight = [4, 3, 2, 1, 7]
+
+s = sum(weight)
+
+nwt = [weight[i]/s for i in range(dmsn)]
+
+
+
+run = True
+
+while run:
+	run = False
+
+
+
+
