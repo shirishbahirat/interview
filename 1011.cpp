3ab9fac6c570646d76455bb784cf3a07139f80f2
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-22 11:11:11 -0600

    message

--- 1011.cpp
+++ 1011.cpp
@@ -35,7 +35,7 @@ public:
     }
 
     int shipWithinDays(vector<int>& weights, int days) {
-        
+
         int left = *max_element(weights.begin(), weights.end());
 
         int right = reduce(weights.begin(), weights.end());
