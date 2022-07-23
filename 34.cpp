75ded39c4b8875194df8b6a00745a2342edbbc3b
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-23 09:01:57 -0600

    solved problems

--- 34.cpp
+++ 34.cpp
@@ -51,7 +51,7 @@ public:
             if (target > nums[mid])
             {
                 left = mid + 1;
-            } 
+            }
             else if ( target < nums[mid])
             {
                 right = mid;
