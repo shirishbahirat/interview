3340646c43e0aab8d2750a2821aa9665685c7632
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-26 21:17:55 -0600

    solved problems

--- kth-ll.cpp
+++ kth-ll.cpp
@@ -62,16 +62,13 @@ void print_last(node** head, int index)
 
         cout << count << endl;
 
-        /*
-
-        if (*kth)
+        if (count > index)
         {
             *kth = (*kth)->next;
 
             cout << (*kth)->data << endl;
         }
 
-        */
         if (count == index)
         {
             kth = head;
