93eaf41a21d1a605fd3de9cc6c6da19f38fe96cb
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-28 20:51:19 -0600

    solved problems

--- 105.cpp
+++ 105.cpp
@@ -9,7 +9,6 @@ struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
 
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
