921d985cc4be127b3e8a1909c8884f563fc8c73f
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-28 20:50:37 -0600

    solved problems

--- 105.cpp
+++ 105.cpp
@@ -7,8 +7,9 @@ using namespace std;
 
 struct TreeNode {
     int val;
-    
+
     TreeNode *left;
+    
     TreeNode *right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
