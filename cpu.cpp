16908e57f09e7612aa18160a8336122b2250dad9
Shirish Bahirat <shirishbahirat@comcast.net>
2022-08-01 11:11:11 -0600

    solved problems

--- cpu.cpp
+++ cpu.cpp
@@ -9,17 +9,18 @@ void data(int m)
 }
 
 
+/*
 dpush() {
     git add .
     if [ "$1" != "" ]
     then
-        git commit -m "$1"
+        git commit --date='$1 11:11:11' --file comment.txt
     else
         git commit --file comment.txt
     fi
     git push
 }
-
+*/
 
 int main(int argc, const char* argv[])
 {
