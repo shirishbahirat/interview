cd51f69e3da17cc457567ed1cd96a12d8b7446c1
Shirish Bahirat <shirishbahirat@comcast.net>
2022-08-14 13:39:22 -0600

    solved problems

---
+++ 44.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int max_sum(int arr[])
+{
+
+
+	return 0;
+
+}
+
+int main(int argc, char const* argv[])
+{
+
+	int arr[] = {34, -50, 42, 14, -5, 86};
+
+
+	return 0;
+}
