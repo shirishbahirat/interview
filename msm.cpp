64f2e73c2139ae896c419caaf1f171d6bf7ce4a9
Shirish Bahirat <shirishbahirat@comcast.net>
2022-08-15 18:32:42 -0600

    solved problems

--- msm.cpp
+++ msm.cpp
@@ -3,26 +3,32 @@
 using namespace std;
 
 struct api {
-	void (*dispatch) (int);
+    void (*dispatch) (int);
 };
 
 
 void send(int idx)
 {
-	cout << "send " << idx << endl;
+    cout << "send " << idx << endl;
 }
 
 int main(int argc, const char* argv[])
 {
 
-	api* arr[] = {nullptr, nullptr, nullptr, nullptr};
+    api* arr[] = {nullptr, nullptr, nullptr, nullptr};
 
-	for (int i = 0; i < 4; ++i)
-	{
-		arr[i] = new api();
-		arr[i]->dispatch = send;
-	}
+    for (int i = 0; i < 4; ++i)
+    {
+        arr[i] = new api();
+        arr[i]->dispatch = send;
+    }
+
+    for (int i = 0; i < 4; ++i)
+    {
+        arr[i] = new api();
+        arr[i]->dispatch = send;
+    }
 
 
-	return 0;
+    return 0;
 }
