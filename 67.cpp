d525f3a0cc0383e8c0209e8e7ba0c8e6acd10980
Shirish Bahirat <shirishbahirat@comcast.net>
2022-08-19 11:11:11 -0600

    solved problems

--- 67.cpp
+++ 67.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <map>
+
 
 using namespace std;
+
+struct node {
+	int data;
+	int idx;
+};
+
+
 
 int main(int argc, const char* argv[])
 {
 
+	node arr[10];
+	int size = 10;
+	int idx = 0;
+	int lfu = 0;
 
 
 
