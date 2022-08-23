59c62d81472833221811e14d545efb7733355ce4
Shirish Bahirat <shirishbahirat@comcast.net>
2022-08-23 11:11:11 -0600

    solved problems

--- 70.cpp
+++ 70.cpp
@@ -7,7 +7,7 @@ int round(int number)
 
 	while(number)
 	{
-		number =- (number%10)
+		number =- (number%10);
 		cout << number << endl;
 	}
 
@@ -17,7 +17,9 @@ int round(int number)
 int main(int argc, const char* argv[])
 {
 
-
+	round(8);
+	round(35);
+	round(64);
 
 
 	return 0;
