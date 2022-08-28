f951d3205ac86d6ffc4a1d78d46321e59d90c702
Shirish Bahirat <shirishbahirat@comcast.net>
2022-08-28 11:11:11 -0600

    solved problems

--- 66.cpp
+++ 66.cpp
@@ -2,21 +2,9 @@
 
 using namespace std;
 
-int round(int number)
+int toss_biased(void)
 {
 
-	int sum = 0;
-
-	while(number)
-	{
-		cout << number%10 << endl;
-
-		sum += (number%10);
-
-		number /= (10);
-	}
-
-	cout << "ans " << 10 - sum << endl;
 
 	return 0;
 }
@@ -24,9 +12,6 @@ int round(int number)
 int main(int argc, const char* argv[])
 {
 
-	round(8);
-	round(35);
-	round(64);
 
 	return 0;
 }
