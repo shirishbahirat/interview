ed8b793ef5b1893405b11d06f5c5eb89e3943e00
Shirish Bahirat <shirishbahirat@comcast.net>
2022-08-29 11:11:11 -0600

    solved problems

---
+++ 65.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int toss_biased(void)
+{
+
+    int data = rand();
+
+    cout << (float)data/RAND_MAX << " " << data << endl;
+
+    return 0;
+}
+
+int main(int argc, const char* argv[])
+{
+
+    toss_biased();
+
+    return 0;
+}
