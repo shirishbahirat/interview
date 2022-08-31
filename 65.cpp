ff33c7e26f71c7cf93bc07b167f99d8a673f2688
Shirish Bahirat <shirishbahirat@comcast.net>
2022-08-31 11:11:11 -0600

    solved problems

--- 65.cpp
+++ 65.cpp
@@ -16,7 +16,7 @@ int toss_biased(void)
 int main(int argc, const char* argv[])
 {
 
-    int arr[4][] =  {{1,  2,  3,  4,  5},
+    int arr[4][5] =  {{1,  2,  3,  4,  5},
                     {6,  7,  8,  9,  10},
                     {11, 12, 13, 14, 15},
                     {16, 17, 18, 19, 20}};
