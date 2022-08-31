4ccac8a096a7d2e356784ac88c2a8b395bee77ae
Shirish Bahirat <shirishbahirat@comcast.net>
2022-08-31 11:11:11 -0600

    solved problems

--- 65.cpp
+++ 65.cpp
@@ -16,6 +16,11 @@ int toss_biased(void)
 int main(int argc, const char* argv[])
 {
 
+    int arr[][] =  {{1,  2,  3,  4,  5},
+                    {6,  7,  8,  9,  10},
+ {11, 12, 13, 14, 15},
+ {16, 17, 18, 19, 20}};
+
     toss_biased();
 
     return 0;
