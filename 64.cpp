ea53cf332550da736c1c0bc03c1801b4081d95ff
Shirish Bahirat <shirishbahirat@comcast.net>
2022-09-03 11:11:11 -0600

    solved problems

---
+++ 64.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int knight(int *arr, int m, int n)
+{
+
+    for (int i = 0; i < m; i++)
+     {
+        for (int j = 0; j < n; j++)
+        {
+           cout << *((arr + i * n) + j) << " ";
+        }
+        cout << endl;
+     }
+
+    return 0;
+}
+
+int main(int argc, const char* argv[])
+{
+
+    int arr[4][5] =  {{1,  2,  3,  4,  5},
+                    {6,  7,  8,  9,  10},
+                    {11, 12, 13, 14, 15},
+                    {16, 17, 18, 19, 20}};
+
+    knight((int *)arr, 5, 4);
+
+    return 0;
+}
